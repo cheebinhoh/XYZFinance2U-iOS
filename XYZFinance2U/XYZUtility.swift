2295f5175e87774766fa7b5769415d9ecd14d077
cb Hoh <hohcheebin@gmail.com>
2018-01-26 21:54:45 -0500

    .

--- XYZFinance2U/XYZUtility.swift
+++ XYZFinance2U/XYZUtility.swift
@@ -715,6 +715,35 @@ func fetchiCloudZoneChange(_ zones: [CKRecordZone],
     opZoneChange.recordZoneChangeTokensUpdatedBlock = { (zoneId, token, data) in
         
         print("----- token \(String(describing: token))")
+        OperationQueue.main.addOperation {
+            
+            for icloudzone in icloudZones {
+                
+                if let zName = icloudzone.value(forKey: XYZiCloudZone.name) as? String, zName == zoneId.zoneName {
+                    
+                    var hasChangeToken = true;
+                    
+                    if let data = icloudzone.value(forKey: XYZiCloudZone.changeToken) as? Data {
+                        
+                        let previousChangeToken = (NSKeyedUnarchiver.unarchiveObject(with: data) as? CKServerChangeToken)
+                        hasChangeToken = previousChangeToken != token!
+                    }
+                    
+                    if hasChangeToken {
+                        
+                        let lastTokenFetchDate = Date()
+                        
+                        let archivedChangeToken = NSKeyedArchiver.archivedData(withRootObject: token! )
+                        icloudzone.setValue(archivedChangeToken, forKey: XYZiCloudZone.changeToken)
+                        icloudzone.setValue(lastTokenFetchDate, forKey: XYZiCloudZone.changeTokenLastFetch)
+                    }
+                    
+                    break
+                }
+                
+                saveManageContext() // save changed token and data
+            }
+        }
     }
     
     opZoneChange.recordZoneFetchCompletionBlock = { (zoneId, changeToken, _, _, error) in
