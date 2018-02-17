acd6a3a50fb8fa82497c121e074f38e17657fee5
cb Hoh <hohcheebin@gmail.com>
2018-02-16 19:20:38 -0500

    .

--- XYZFinance2U/Budget/BudgetDetailTableViewController.swift
+++ XYZFinance2U/Budget/BudgetDetailTableViewController.swift
@@ -16,7 +16,17 @@ protocol BudgetDetailDelegate: class {
 }
 
 class BudgetDetailTableViewController: UITableViewController,
-    BudgetSelectionDelegate {
+    BudgetSelectionDelegate,
+    BudgetDetailTextTableViewCellDelegate {
+    
+    func textDidEndEditing(_ sender: BudgetDetailTextTableViewCell) {
+        
+    }
+    
+    func textDidBeginEditing(_ sender: BudgetDetailTextTableViewCell) {
+    
+    }
+    
     
     func budgetSelected(newBudget: XYZBudget?) {
 
@@ -30,6 +40,11 @@ class BudgetDetailTableViewController: UITableViewController,
     var isPopover: Bool = false
     var isPushinto: Bool = false
     var modalEditing = false
+    var budget: XYZBudget?
+    var sectinList = [TableSectionCell]()
+    var budgetType = ""
+    var amount = 0.0
+    var currencyCode = Locale.current.currencyCode
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
         // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
@@ -52,6 +67,65 @@ class BudgetDetailTableViewController: UITableViewController,
         }
     }
     
+    @IBAction func save(_ sender: Any) {
+        
+        /*
+        if isPushinto {
+            
+            fatalError("Exception: todo")
+            
+            //saveData()
+            //expenseDelegate?.saveExpense(expense: expense!)
+            //navigationController?.popViewController(animated: true)
+        } else if isPopover {
+            
+            if nil == income {
+                
+                income = XYZAccount(id: nil, sequenceNr: 0, bank: bank, accountNr: accountNr, amount: amount!, date: date!, context: managedContext())
+                
+                saveData()
+                incomeDelegate?.saveNewIncome(income: income!)
+            } else {
+                
+                saveData()
+                incomeDelegate?.saveIncome(income: income!)
+            }
+            
+            dismiss(animated: true, completion: nil)
+        } else {
+            
+            saveData()
+            navigationItem.leftBarButtonItem?.isEnabled = false
+            modalEditing = false
+            loadDataInTableSectionCell()
+            tableView.reloadData()
+            
+            let masterViewController = getMasterTableViewController()
+            
+            let editButton = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(edit(_:)))
+            navigationItem.setRightBarButton(editButton, animated: true)
+            navigationItem.leftBarButtonItem = nil
+            
+            masterViewController.navigationItem.leftBarButtonItem?.isEnabled = true
+            masterViewController.navigationItem.rightBarButtonItem?.isEnabled = true
+            
+            incomeDelegate?.saveIncome(income: income!)
+        }*/
+    }
+    
+    func loadDataIntoSectionList() {
+    
+        sectinList = [TableSectionCell]()
+        
+        let mainSection = TableSectionCell(identifier: "main", title: nil, cellList: ["budget", "amount"], data: nil)
+        sectinList.append(mainSection)
+    }
+    
+    func loadData() {
+        
+        loadDataIntoSectionList()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -59,6 +133,30 @@ class BudgetDetailTableViewController: UITableViewController,
         
         navigationItem.largeTitleDisplayMode = .never
         
+        if let split = self.parent?.parent as? UISplitViewController {
+            
+            if !split.isCollapsed {
+                
+                navigationItem.rightBarButtonItem = nil
+                navigationItem.leftBarButtonItem = nil
+                
+                modalEditing = false
+            }
+        }
+        
+        if isPopover {
+            
+            let saveButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(save(_:)))
+            navigationItem.setRightBarButton(saveButton, animated: true)
+        }
+        
+        loadData()
+        
+        if let _ = budget {
+            
+            navigationItem.title = "Budget"
+        }
+
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -78,26 +176,76 @@ class BudgetDetailTableViewController: UITableViewController,
     }
 
     // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        if section == 0 {
+            
+            return 35
+        } else {
+            
+            return 17.5
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        return sectinList[section].title
+    }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+
+        return sectinList.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+
+        return sectinList[section].cellList.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell: UITableViewCell
+        
+        switch sectinList[indexPath.section].cellList[indexPath.row] {
+        
+            case "budget":
+                guard let textcell = tableView.dequeueReusableCell(withIdentifier: "budgetDetailTextCell", for: indexPath) as? BudgetDetailTextTableViewCell else {
+                    
+                    fatalError("Exception: budgetDetailTextCell is failed to be created")
+                }
+            
+                textcell.input.translatesAutoresizingMaskIntoConstraints = false
+                textcell.input.isEnabled = modalEditing
+                textcell.delegate = self
+                textcell.input.placeholder = "budget type"
+                textcell.input.text = budgetType
+                textcell.label.text = "Budget"
+                
+                cell = textcell
+            
+            
+            case "amount":
+                guard let textcell = tableView.dequeueReusableCell(withIdentifier: "budgetDetailTextCell", for: indexPath) as? BudgetDetailTextTableViewCell else {
+                    
+                    fatalError("Exception: budgetDetailTextCell is failed to be created")
+                }
 
-        // Configure the cell...
-
+                textcell.input.isEnabled = modalEditing
+                textcell.delegate = self
+                textcell.enableMonetaryEditing(true, currencyCode!)
+                
+                textcell.input.placeholder = formattingCurrencyValue(input: 0.0, code: currencyCode)
+                textcell.input.text = formattingCurrencyValue(input: amount, code: currencyCode)
+                textcell.label.text = "Amount"
+                
+                cell = textcell
+            
+            default:
+                fatalError("Exception: \(sectinList[indexPath.section].cellList[indexPath.row]) not handle")
+        }
+        
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
---
+++ XYZFinance2U/Budget/BudgetDetailTextTableViewCell.swift
@@ -0,0 +1,91 @@
+//
+//  BudgetDetailTextTableViewCell.swift
+//  XYZFinance2U
+//
+//  Created by Chee Bin Hoh on 2/16/18.
+//  Copyright © 2018 CB Hoh. All rights reserved.
+//
+
+import UIKit
+
+protocol BudgetDetailTextTableViewCellDelegate : class {
+    
+    func textDidEndEditing(_ sender:BudgetDetailTextTableViewCell)
+    func textDidBeginEditing(_ sender:BudgetDetailTextTableViewCell)
+}
+
+class BudgetDetailTextTableViewCell: UITableViewCell,
+    UITextFieldDelegate {
+
+    // MARK: - property
+    
+    weak var delegate: BudgetDetailTextTableViewCellDelegate?
+    var currencyCode: String = Locale.current.currencyCode!
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.input.delegate = self
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        delegate?.textDidBeginEditing(self)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        delegate?.textDidEndEditing(self)
+    }
+    
+    func enableMonetaryEditing(_ enanble: Bool, _ currencyCode: String) {
+        
+        if enanble {
+            
+            self.currencyCode = currencyCode
+            input.addDoneToolbar(onDone: nil)
+            input.clearButtonMode = .never
+            input.keyboardType = .numberPad
+            input.text = formattingCurrencyValue(input: 0.0, code: self.currencyCode)
+            input.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        } else {
+            
+            input.keyboardType = .default
+            input.text = ""
+            input.addTarget(nil, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        }
+    }
+    
+    // MARK: - IBAction
+    
+    @objc
+    func textFieldDidChange(_ textField: UITextField) {
+        
+        var text = textField.text ?? "0.00"
+        
+        text = formattingDoubleValue(input: text)
+        text = formattingAndProcessDoubleValue(input: text)
+        text = formattingCurrencyValue(input: text, code: currencyCode)
+        textField.text = text
+        delegate?.textDidEndEditing(self)
+    }
+}
