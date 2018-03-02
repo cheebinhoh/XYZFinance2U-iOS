e89ab4de6ee1265220a5af2eb73cf6aa9ce78655
cb Hoh <hohcheebin@gmail.com>
2018-03-01 23:06:44 -0500

    /

--- XYZFinance2U/Budget/CalendarCollectionViewController.swift
+++ XYZFinance2U/Budget/CalendarCollectionViewController.swift
@@ -316,6 +316,8 @@ class CalendarCollectionViewController: UICollectionViewController,
         
         super.viewDidLoad()
 
+        collectionView?.isScrollEnabled = false
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
