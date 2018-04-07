e2ae3b98b1b91348113d14ae52c623efcadf08e3
cb Hoh <hohcheebin@gmail.com>
2018-04-06 20:26:03 -0400

    .

--- XYZFinance2U/Budget/BudgetListTableViewController.swift
+++ XYZFinance2U/Budget/BudgetListTableViewController.swift
@@ -185,6 +185,7 @@ class BudgetListTableViewController: UITableViewController {
     }
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
@@ -192,12 +193,12 @@ class BudgetListTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+
         return cellList.count
     }
 
