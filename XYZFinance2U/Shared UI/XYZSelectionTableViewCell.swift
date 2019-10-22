4e9003ecd82c2742a5d026c04937d979d685092a
cb Hoh <hohcheebin@gmail.com>
2019-10-21 20:28:04 -0400

    .

--- XYZFinance2U/Shared UI/XYZSelectionTableViewCell.swift
+++ XYZFinance2U/Shared UI/XYZSelectionTableViewCell.swift
@@ -18,6 +18,7 @@ class XYZSelectionTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         
         super.awakeFromNib()
+        
         // Initialization code
     }
     
