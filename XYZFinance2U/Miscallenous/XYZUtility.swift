d75a3cfb04cb4cd5c99b89fec6be3705b559d772
cb Hoh <hohcheebin@gmail.com>
2020-10-07 19:39:49 -0400

    .

--- XYZFinance2U/Miscallenous/XYZUtility.swift
+++ XYZFinance2U/Miscallenous/XYZUtility.swift
@@ -3,7 +3,7 @@
 //  XYZFinance2U
 //
 //  Created by Chee Bin Hoh on 11/30/17.
-//  Copyright © 2017 - 2020 - 2020 Chee Bin Hoh. All rights reserved.
+//  Copyright © 2017 - 2020 Chee Bin Hoh. All rights reserved.
 //
 
 import Foundation
